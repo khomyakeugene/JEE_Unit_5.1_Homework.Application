82cfec6716f0618044be9f5c0ebdd3f26f33f298
khomyakeugene <khomyak_eugene@yahoo.com>
2016-04-23 20:18:48 +0300

    JEE Unit 5.1 Homework: Application

--- src/main/java/com/company/Bootstrap.java
+++ src/main/java/com/company/Bootstrap.java
@@ -3,15 +3,14 @@ package com.company;
 import com.company.calculator.AppConfig;
 import com.company.calculator.CalculatorLauncher;
 import com.company.calculator.SimpleCalculator;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Bootstrap {
+    private final static String CALCULATOR_BEAN_NAME = "simpleCalculator";
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        SimpleCalculator simpleCalculator = applicationContext.getBean("simpleCalculator", SimpleCalculator.class);
-
-        new CalculatorLauncher().interactiveCalculation(simpleCalculator);
+        new CalculatorLauncher().interactiveCalculation(
+                new AnnotationConfigApplicationContext(AppConfig.class).
+                        getBean(CALCULATOR_BEAN_NAME, SimpleCalculator.class));
     }
 }
--- src/main/java/com/company/calculator/NumberMultiplication.java
+++ src/main/java/com/company/calculator/NumberMultiplication.java
@@ -3,7 +3,7 @@ package com.company.calculator;
 /**
  * Created by Yevhen on 23.04.2016.
  */
-public class NumberMultiplication extends NumberOperation {
+public class NumberMultiplication extends BinaryNumberOperation implements Operation {
     @Override
     protected String calculate() {
         return Double.toString(getDoubleOperand(0) * getDoubleOperand(1));
