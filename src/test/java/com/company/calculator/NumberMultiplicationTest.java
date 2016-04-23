4ae968d9c7585cd63d7e16069e021817034f22cc
khomyakeugene <khomyak_eugene@yahoo.com>
2016-04-23 17:13:56 +0300

    JEE Unit 5.1 Homework: Application

---
+++ src/test/java/com/company/calculator/NumberMultiplicationTest.java
@@ -0,0 +1,23 @@
+package com.company.calculator;
+
+import org.junit.BeforeClass;
+
+/**
+ * Created by Yevhen on 23.04.2016.
+ */
+public class NumberMultiplicationTest extends NumberTest {
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        numberOperation = new NumberMultiplication();
+    }
+
+    @Override
+    protected int calcIntExpected(int operand_1, int operand_2) {
+        return operand_1 * operand_2;
+    }
+
+    @Override
+    protected double calcDoubleExpected(double operand_1, double operand_2) {
+        return operand_1 * operand_2;
+    }
+}
---
+++ src/test/java/com/company/calculator/NumberTest.java
@@ -0,0 +1,46 @@
+package com.company.calculator;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Yevhen on 23.04.2016.
+ */
+public abstract class NumberTest {
+    protected static NumberOperation numberOperation;
+
+    protected abstract int calcIntExpected(int operand_1, int  operand_2);
+
+    protected abstract double calcDoubleExpected(double operand_1, double  operand_2);
+
+    private void executeDoubleTest() throws Exception {
+        double operand_1 = Util.getRandomDouble();
+        double operand_2 = Util.getRandomDouble();
+
+        ArrayList<String> operands = new ArrayList<>();
+        operands.add(Double.toString(operand_1));
+        operands.add(Double.toString(operand_2));
+        numberOperation.setOperands(operands);
+
+        assertEquals(Double.toString(calcDoubleExpected(operand_1, operand_2)), numberOperation.execute());
+    }
+
+    private void executeIntegerTest() throws Exception {
+        int operand_1 = Util.getRandomInteger();
+        int operand_2 = Util.getRandomInteger();
+
+        ArrayList<String> operands = new ArrayList<>();
+        operands.add(Integer.toString(operand_1));
+        operands.add(Integer.toString(operand_2));
+        numberOperation.setOperands(operands);
+
+        assertEquals(Integer.toString(calcIntExpected(operand_1, operand_2)), numberOperation.execute());
+    }
+
+    @org.junit.Test(timeout = 1000)
+    public void executeTest() throws Exception {
+        executeIntegerTest();
+        executeDoubleTest();
+    }
+}
