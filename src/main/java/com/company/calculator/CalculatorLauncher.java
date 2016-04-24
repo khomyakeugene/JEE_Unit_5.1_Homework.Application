045478de7e6369e8ddcd66b4097fe4f108d7027e
khomyakeugene <khomyak_eugene@yahoo.com>
2016-04-24 10:51:12 +0300

    JEE Unit 5.1 Homework: Application

--- src/main/java/com/company/calculator/CalculatorLauncher.java
+++ src/main/java/com/company/calculator/CalculatorLauncher.java
@@ -2,11 +2,13 @@ package com.company.calculator;
 
 import com.company.utils.Utils;
 
+import java.util.Arrays;
+
 /**
  * Created by Yevhen on 23.04.2016.
  */
 public class CalculatorLauncher {
-    private static final String PROMPT_MESSAGE = "Please, enter a formula (or press only <Enter> to stop):";
+    private static final String PROMPT_MESSAGE = "You can execute the following operations: %s. Please, enter a formula (or press only <Enter> to stop):";
     private static final String FAREWELL_MESSAGE = "Thank you for use our calculator";
     private static final String RESULT_PATTERN = "%s = %s";
 
@@ -14,7 +16,8 @@ public class CalculatorLauncher {
         String expression;
 
         do {
-            expression = Utils.readInputString(PROMPT_MESSAGE);
+            expression = Utils.readInputString(String.format(PROMPT_MESSAGE,
+                    Arrays.toString(simpleCalculator.operationCodeList())));
             if (!expression.isEmpty()) {
                 try {
                     Utils.printMessage(String.format(RESULT_PATTERN, expression, simpleCalculator.execute(expression)));
