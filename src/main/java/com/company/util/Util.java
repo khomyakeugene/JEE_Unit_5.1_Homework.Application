3ecf6daa2719991582edd5b5e01147964153e564
khomyakeugene <khomyak_eugene@yahoo.com>
2016-04-28 23:29:57 +0300

    JEE Unit 5.2 Homework: Application

--- src/main/java/com/company/util/Util.java
+++ src/main/java/com/company/util/Util.java
@@ -96,19 +96,4 @@ public class Util {
 
         return result;
     }
-
-    public static String getApplicationName() {
-        String result;
-
-        try {
-            result = getApplicationMainClass().getProtectionDomain().getCodeSource().getLocation().getFile();
-        } catch (NullPointerException e) {
-            // Unfortunately, it is difficult to recognize why when it is executed "under IntelliJ IDEA",
-            // class.getProtectionDomain() could return null, but, in this case, there is kind of compromise -
-            // just let return the name of the main class
-            result = getApplicationMainClassName();
-        }
-
-        return result;
-    }
 }
